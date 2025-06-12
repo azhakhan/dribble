4bf6c821b73e9dc78a39a8aad2b599e69152ed20
Ayazhan Zhakhan <az@dropbase.io>
2025-06-12 18:07:00 -0400

    updated capybara.riv

--- client/src/components/Capybara.tsx
+++ client/src/components/Capybara.tsx
@@ -3,13 +3,13 @@ import { useRive } from "@rive-app/react-canvas";
 export const Capybara = () => {
   const { RiveComponent } = useRive({
     src: "/capybara.riv",
-    artboard: "capybarra",
+    artboard: "capybara",
     stateMachines: "State Machine 1",
     autoplay: true
   });
 
   return (
-    <div className="w-full h-15 overflow-hidden flex items-center justify-center">
+    <div className="w-full h-20 overflow-hidden flex items-center justify-center">
       <RiveComponent className="w-full h-full" />
     </div>
   );
