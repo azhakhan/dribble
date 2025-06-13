62eda5f691972669a9518189bdd218a8dba16b51
Ayazhan Zhakhan <az@dropbase.io>
2025-06-13 15:22:28 -0400

    added capybara light mode

--- client/src/components/Capybara.tsx
+++ client/src/components/Capybara.tsx
@@ -1,16 +1,24 @@
 import { useRive } from "@rive-app/react-canvas";
+import { useTheme } from "@/components/theme-provider";
 
 export const Capybara = () => {
+  const { theme } = useTheme();
+
+  let themeColor = theme;
+  if (theme === "system") {
+    themeColor = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+  }
+
   const { RiveComponent } = useRive({
     src: "/capybara.riv",
-    artboard: "capybara",
+    artboard: themeColor === "light" ? "capybara-light" : "capybara",
     stateMachines: "State Machine 1",
     autoplay: true
   });
 
   return (
     <div className="w-full h-20 overflow-hidden flex items-center justify-center">
-      <RiveComponent className="w-full h-full" />
+      <RiveComponent key={theme} className="w-full h-full" />
     </div>
   );
 };
