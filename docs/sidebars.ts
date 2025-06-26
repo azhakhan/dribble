d883abd86c9ae4895145a62adba13bdc67b3c498
Ayazhan Zhakhan <az@dropbase.io>
2025-06-26 14:17:47 -0400

    fixed the issue with missing sidebar, deployed on netlify

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -15,6 +15,7 @@ import type { SidebarsConfig } from "@docusaurus/plugin-content-docs";
 const sidebars: SidebarsConfig = {
   // Organized documentation sidebar
   tutorialSidebar: [
+    "intro",
     {
       type: "category",
       label: "Getting Started",
